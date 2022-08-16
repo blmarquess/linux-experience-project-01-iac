19161f9712d8fc64d4801a729cd6c7daff79fe3e
Bruno Marques <blmarques.dev@gmail.com>
2022-08-16 00:37:19 -0300

    feat(folders): created a routine for creating folders and setting their permissions

--- setup_iac.sh
+++ setup_iac.sh
@@ -24,6 +24,12 @@ users_group_sec=(
   rogerio
 )
 
+folders_list=(
+  adm
+  ven
+  sec
+  publico
+)
 
 echo "Inicianando a rotina de criação de usuários e grupos"
 
@@ -59,3 +65,30 @@ create_users() {
 
 echo "Crindo usuarios e adicionando aos seus respectivos grupos."
 create_users
+
+echo "Rotina de criação de usuários e grupos finalizada!"
+
+echo "Criando o diretórios dos grupos..."
+
+
+for folder in ${folders_list[@]}; do
+  mkdir /home/$folder
+  echo "Diretório criado -> /home/$folder"
+done
+
+echo "Definindo propriedade e permissões aos diretórios..."
+
+chown -R root:${group_list[0]} /home/adm
+chown -R root:${group_list[1]} /home/ven
+chown -R root:${group_list[2]} /home/sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Diretórios criados com sucesso!"
+
+echo "Rotina de criação de diretórios finalizada!"
+
+echo "Setup finalizada!"
