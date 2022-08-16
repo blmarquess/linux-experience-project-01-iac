533cfce4cff2f5d694ec4a8bda7ffb157148604e
Bruno Marques <blmarques.dev@gmail.com>
2022-08-16 00:00:10 -0300

    feat(users_groups): created list of groups and users

---
+++ setup_iac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+group_list=(
+  GRP_ADM
+  GRP_VEN
+  GRP_SEC
+)
+
+users_group_adm=(
+  carlos
+  maria
+  joao_
+)
+
+users_group_ven=(
+  debora
+  sebastiana
+  roberto
+)
+
+users_group_sec=(
+  josefina
+  amanda
+  rogerio
+)
+
+
