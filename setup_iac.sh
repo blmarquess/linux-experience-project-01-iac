370b7c3ead4ca545a3a984acb3a1c0ef45b3ef1e
Bruno Marques <blmarques.dev@gmail.com>
2022-08-16 00:24:38 -0300

    feat(users): created the user creation routine
    users created with default password and defined permission groups

--- setup_iac.sh
+++ setup_iac.sh
@@ -33,4 +33,29 @@ for group in ${group_list[@]}; do
   groupadd $group
 done
 
+create_users() {
+  echo "Crindo usuarios do grupo de administradores..."
 
+  for adm_user in ${users_group_adm[@]}; do
+    useradd -m -s /bin/bash $adm_user -p $(openssl passwd -crypt Senha123) -G ${group_list[0]}
+    echo "Criado o usuario $adm_user com sucesso e adicionado ao grupo -> ${group_list[1]}"
+  done
+
+  echo "Crindo usuarios do grupo de vendas..."
+
+  for ven_user in ${users_group_ven[@]}; do
+    useradd -m -s /bin/bash $ven_user -p $(openssl passwd -crypt Senha123) -G ${group_list[1]}
+    echo "Criado o usuario $ven_user com sucesso e adicionado ao grupo -> ${group_list[1]}"
+  done
+
+  echo "Crindo usuarios do grupo de segurança..."
+  for sec_user in ${users_group_sec[@]}; do
+    useradd -m -s /bin/bash $sec_user -p $(openssl passwd -crypt Senha123) -G ${group_list[2]}
+    echo "Criado o usuario $sec_user com sucesso e adicionado ao grupo -> ${group_list[2]}"
+  done
+
+  echo "Usuarios criados com sucesso!"
+}
+
+echo "Crindo usuarios e adicionando aos seus respectivos grupos."
+create_users
